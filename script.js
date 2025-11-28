a2173568e929ab741ee143da6e3a10ca4994b68d
tassnyme512 <tassnymesoussi@gmail.com>
2025-11-27 20:47:15 -0800

    2eme commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const UNSPLASH_ACCESS_KEY = "ND1op4H2pWsat5LNMc1F2CRNDWh5G532N0mKvwERhxA";
+const PER_PAGE = 12;
+// DOM
+const searchForm = document.getElementById("searchForm");
+const searchInput = document.getElementById("searchInput");
+const orientationSelect = document.getElementById("orientation");
+const gallery = document.getElementById("gallery");
+const status = document.getElementById("status");
+const loadMoreBtn = document.getElementById("loadMore");
+const loadMoreWrap = document.getElementById("loadMoreWrap");
+
 document.getElementById("year").textContent = new Date().getFullYear();
